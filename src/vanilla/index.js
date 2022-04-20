299ff5abbc18e53991e14583cc5419e2e514cb70
Ankake <ankake_1022@i.softbank.jp>
2022-04-20 14:38:15 +0900

    send test

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -6,6 +6,20 @@ document.addEventListener("DOMContentLoaded", function() {
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
         console.log("Success! you can do something with LIFF API here.")
+        document.getElementById('send').addEventListener('click', ()=> {
+            if (!liff.isInClient()) {
+              document.getElementById('log').value += 'sendMessagesText ng\n';
+            } else {
+              liff.sendMessages([{
+                'type': 'text',
+                'text': document.getElementById('message').value
+              }]).then(function() {
+                document.getElementById('log').value += 'sendMessagesText completed\n';
+              }).catch(function(error) {
+                document.getElementById('log').value += 'sendMessagesText()=' + error + '\n';
+              });
+            }
+          });
     })
     .catch((error) => {
         console.log(error)

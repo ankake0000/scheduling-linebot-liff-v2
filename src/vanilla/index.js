9bd1923f47c7cc4fc16409b8e9aa1f27449d3bea
Ankake <ankake_1022@i.softbank.jp>
2022-04-20 15:47:49 +0900

    send test

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -8,20 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Success! you can do something with LIFF API here.")
         document.getElementById('send').addEventListener('click', ()=> {
             getProfile();
-            if (!liff.isInClient()) {
-              document.getElementById('log').value += 'sendMessagesText ng\n';
-            } else {
-              liff
-              .init({ liffId: process.env.LIFF_ID })
-              .sendMessages([{
-                'type': 'text',
-                'text': document.getElementById('message').value
-              }]).then(function() {
-                document.getElementById('log').value += 'sendMessagesText completed\n';
-              }).catch(function(error) {
-                document.getElementById('log').value += 'sendMessagesText()=' + error + '\n';
-              });
-            }
+            sendMessages();
         });
     })
     .catch((error) => {
@@ -31,7 +18,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function getProfile(){
     liff.getProfile().then(function (profile) {
-        document.getElementById('log').value =  profile.displayName + '\n';
+        document.getElementById('log').value +=  profile.displayName + '\n';
         document.getElementById('log').value += profile.userId;
     });
+}
+
+function sendMessages(){
+    if (!liff.isInClient()) {
+        document.getElementById('log').value += 'sendMessagesText ng\n';
+      } else {
+        liff
+        .init({ liffId: process.env.LIFF_ID })
+        .sendMessages([{
+          'type': 'text',
+          'text': document.getElementById('message').value
+        }]).then(function() {
+          document.getElementById('log').value += 'sendMessagesText completed\n';
+        }).catch(function(error) {
+          document.getElementById('log').value += 'sendMessagesText()=' + error + '\n';
+        });
+      }
 }

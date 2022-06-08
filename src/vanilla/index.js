50ef8adace5ad105355a2c8782d6e246e1bd1728
Ankake <ankake_1022@i.softbank.jp>
2022-06-08 11:15:42 +0900

    id display

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -41,7 +41,8 @@ function sendMessages(message_data){
         // document.getElementById
         let message = "";
         let edit_id = Number($('#plan-id').val);
-        if(Number.isFinite(edit_id)){ message = edit_id.toString() + '\n'}
+        // if(Number.isFinite(edit_id)){ message = edit_id.toString() + '\n'}
+        message = edit_id + '\n'
         Object.keys(message_data).forEach(function(value){
             message += this[value] + '\n';
         }, message_data);

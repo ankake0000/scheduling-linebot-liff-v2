6b3ec2bb487c9f7370e6efd8d7fad908cee46a3a
Ankake <ankake_1022@i.softbank.jp>
2022-06-23 18:20:32 +0900

    remove commentout

--- src/vanilla/design.js
+++ src/vanilla/design.js
@@ -1,6 +1,5 @@
 window.onload = function(){
 
-    
     $('#plan-date').datepicker({
         showButtonPanel: true
         ,currentText: "今日"
@@ -28,56 +27,44 @@ window.onload = function(){
 
     let hour_options_html = "";
     for(let h=0; h<24; h++){
-        const h_normalization = h.toString().padStart(2,'0')
-        hour_options_html += "<option value=" + h_normalization + ">" + h_normalization + "</option>"
+        const h_normalization = h.toString().padStart(2,'0');
+        hour_options_html += "<option value=" + h_normalization + ">" + h_normalization + "</option>";
     }
     $('#hour-select').html(hour_options_html);
-    // let hour = document.getElementById("hour-select");
-    // hour.innerHTML = hour_options_html;
 
     let min_options_html="";
     for(let m=0; m<60; m++){
-        const m_normalization = m.toString().padStart(2,'0')
-        min_options_html += "<option value=" + m_normalization + ">" + m_normalization + "</option>"
+        const m_normalization = m.toString().padStart(2,'0');
+        min_options_html += "<option value=" + m_normalization + ">" + m_normalization + "</option>";
     }
     $('#min-select').html(min_options_html);
-    // let min = document.getElementById("min-select");
-    // min.innerHTML = min_options_html;
 
 
     $('#send-btn').on('click', ()=>{
         check();
     });
-    // let send_btn = document.getElementById('send-btn');
-    // send_btn.addEventListener('click', function(){
-    //     check();
-    // });
 
     $('#plan-title').on('keyup',()=>{
-        formHelperValidation('#plan-title_helper', '#plan-title_invalid-feedback')
+        formHelperValidation('#plan-title_helper', '#plan-title_invalid-feedback');
     })
-    // let plan_title = document.getElementById('plan-title');
-    // plan_title.addEventListener('keyup', () => {
-    //     formHelperValidation('#plan-title_helper', '#plan-title_invalid-feedback')
-    // })
+
 
     // Loop over them and prevent submission
 
     let location_field = location.href.split("?");
 
     if(location_field.length > 1){
-        $('#form-title').text("予定の編集")
+        $('#form-title').text("予定の編集");
         let decode_uri = decodeURIComponent(location_field[1]);
         // URL Persent Decode Required
-        let input_data_field = decode_uri.split("&")
-        let plan_id = input_data_field[0].split("=")[1]
-        let input_plan_title = input_data_field[1].split("=")[1]
-        let input_plan_date = input_data_field[2].split("=")[1]
-        let input_plan_hour = input_data_field[3].split("=")[1]
-        let input_plan_min = input_data_field[4].split("=")[1]
-        let input_plan_desc = input_data_field[5].split("=")[1]
+        let input_data_field = decode_uri.split("&");
+        let plan_id = input_data_field[0].split("=")[1];
+        let input_plan_title = input_data_field[1].split("=")[1];
+        let input_plan_date = input_data_field[2].split("=")[1];
+        let input_plan_hour = input_data_field[3].split("=")[1];
+        let input_plan_min = input_data_field[4].split("=")[1];
+        let input_plan_desc = input_data_field[5].split("=")[1];
 
-        // $('#plan-description').text(decode_uri);
         $('#plan-id').val(plan_id);
         $('#plan-title').val(input_plan_title);
         $('#plan-title').addClass('active');
@@ -89,11 +76,6 @@ window.onload = function(){
         $('#plan-description').addClass('active');
     }
 }
-
-// function planTitleFeedbackStatusReload(){
-//     return document.defaultView.getComputedStyle(document.getElementById('plan-title_invalid-feedback'), null).display
-// }
-
 
 function formHelperValidation(helper_id, invalid_feedback_id){
     if($(invalid_feedback_id).css('display') == 'none' ){
